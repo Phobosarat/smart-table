8760902d964be1b2af2e9d6532b75b1ce29b95f0
Phobos <phobosarat@yandex.com>
2026-03-10 13:25:10 +0400

    revision 2

--- src/components/searching.js
+++ src/components/searching.js
@@ -4,20 +4,18 @@ import {rules, createComparison} from "../lib/compare.js";
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
     const compare = createComparison(
-        {
-            skipEmptyTargetValues: rules.skipEmptyTargetValues
-        },
-        {
-            [searchField]: rules.searchMultipleFields(
+        ['skipEmptyTargetValues'],
+        [
+            rules.searchMultipleFields(
                 searchField,
                 ['date', 'customer', 'seller'],
                 false
             )
-        }
+        ]
     );
 
-    return (data, state, action) => {
+    return (data, state) => {
         // @todo: #5.2 — применить компаратор
         return data.filter((row) => compare(row, state));
-    }
+    };
 }
